535e249a134b53d02cc379f5f12446c4ab788893
Dylan <Dylan@Dylan.local>
2017-04-18 22:31:02 +0800

    删除pod wenjian

--- Pods/Target Support Files/Pods-NNPhotos/Pods-NNPhotos-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NNPhotos : NSObject
-@end
-@implementation PodsDummy_Pods_NNPhotos
-@end
--- Pods/Target Support Files/Pods-NNPhotosUITests/Pods-NNPhotosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NNPhotosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_NNPhotosUITests
-@end
